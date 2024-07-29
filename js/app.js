d3d20dff9af6d653648635579cf9703a63aa401d
veparity <veparity@gmail.com>
2024-07-29 19:34:40 -0400

    Editing JS and minor css

--- js/app.js
+++ js/app.js
@@ -1,43 +1,10 @@
 (function (app) {
   "use strict";
-  // const pageItems = {};
 
   app.portfolioStartup = function () {
     console.log("portfolioStartup called");
-    // pageItems.navbar = document.getElementById("navbar");
-    // pageItems.navbarItems = document.getElementById("navbarItems");
-    // pageItems.navCollapase = document.getElementById("navbarCollapse");
-
-    // window.addEventListener("scroll", changeNavColor);
-    // window.addEventListener("scroll", expandNavText);
-
-    // changeNavColor();
-    // expandNavText();
-
     initIntersectionObserver();
   };
-
-  // function changeNavColor() {
-  // if (window.scrollY > 100) {
-  //   pageItems.navbar.style.backgroundColor = "var(--p-background-color)";
-  //   pageItems.navbar.style.boxShadow =
-  //     "var(--p-box-shadow) 0px 48px 100px 0px";
-  //   pageItems.navbarItems.style.backgroundColor = "var(--p-background-color)";
-  // } else {
-  //   pageItems.navbar.style.backgroundColor = "transparent";
-  //   pageItems.navbar.style.boxShadow = "none";
-  //   pageItems.navbarItems.style.backgroundColor = "transparent";
-  // }
-  // }
-
-  // function expandNavText() {
-  //   if (window.scrollY > 100) {
-  //     pageItems.navbarItems.style.fontSize = "1.2rem";
-  //     pageItems.navbarItems.style.paddingRight = "1rem";
-  //   } else {
-  //     pageItems.navbarItems.style.fontSize = "1rem";
-  //   }
-  // }
 
   function initIntersectionObserver() {
     const animationElements = document.querySelectorAll(".load-in-from-top");
@@ -67,7 +34,6 @@
     });
   }
 
-  // Apply animation immediately to the hero section element if it's in view on load
   function applyInitialAnimation() {
     const heroElement = document.querySelector(".hero .load-in-from-top");
 
